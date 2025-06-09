1d734bc303b984247e3abc6af53eb84f9a2c806e
1Stiva <marcus.stivalletti@souu.unifeb.edu.br]>
2025-06-09 16:41:42 -0300

    Criei a classe Produto e coloquei os atributos nela

---
+++ P2 Marcus 2°ano/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_Marcus_2_ano
+{
+    public class Produto
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public decimal Preço { get; }
+        public string Categoria { get; }
+    }
+}
