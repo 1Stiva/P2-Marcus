d7cbf1a751eb1bd734ea3c519f8ad2030d271210
1Stiva <marcus.stivalletti@souu.unifeb.edu.br]>
2025-06-09 17:54:25 -0300

    Mexendo na classe program.cs criei alguns porodutos, Criei um cliente e adicionei produtos no pedido

--- P2 Marcus 2°ano/Program.cs
+++ P2 Marcus 2°ano/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using P2_Marcus_2_ano;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var produto1 = new Produto(1, "Mouse", 50.00m, "Eletrônico");
+        var produto2 = new Produto(2, "Calça", 30.00m, "Promoção");
+        var produto3 = new Produto(3, "Teclado", 120.00m, "Eletrônco");
+
+        var cliente = new Cliente(1, "Marcus", "Marcus@email.com", "44591286835");
+
+        var itens = new List<ItemPedido>
+        {
+            new ItemPedido(produto1, 2),
+            new ItemPedido(produto2, 3),
+            new ItemPedido(produto3, 1)
+        };
+    }
+}
