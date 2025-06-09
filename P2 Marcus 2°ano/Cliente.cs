48fc60ae78d25cbdc27b83eb1fca96aea9f59f0a
1Stiva <marcus.stivalletti@souu.unifeb.edu.br]>
2025-06-09 17:08:43 -0300

    Utilizei na classe o S do conceito SOLID e dividi em Id para indentificar o cliente, Nome para saber o nome do cliente, Email para saber o email do cliente e CPF para saber o cpf do cliente

--- P2 Marcus 2°ano/Cliente.cs
+++ P2 Marcus 2°ano/Cliente.cs
@@ -11,6 +11,14 @@ namespace P2_Marcus_2_ano
         public int Id { get; }
         public string Nome { get; }
         public string Email { get; }
-        public string Cpf { get; }
+        public string CPF { get; }
+
+        public Cliente(int id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            CPF = cpf;
+        }
     }
 }
