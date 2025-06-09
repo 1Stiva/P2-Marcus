3c0441f06ad5798cb0aa6d50d19c6e3e9a338e8a
1Stiva <marcus.stivalletti@souu.unifeb.edu.br]>
2025-06-09 17:00:21 -0300

    assim como na Classe produto, adicionei os atributos dela

--- P2 Marcus 2°ano/Cliente.cs
+++ P2 Marcus 2°ano/Cliente.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace P2_Marcus_2_ano
 {
-    class Cliente
+    public class Cliente
     {
+        public int Id { get; }
+        public string Nome { get; }
+        public string Email { get; }
+        public string Cpf { get; }
     }
 }
